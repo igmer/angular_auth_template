b614c1825aee50aba37171cf456ac62af6db0062
igmer <igmerdz@gmail.com>
2022-12-14 19:25:14 -0600

    adding navbar

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -3,9 +3,14 @@ import {MatButtonModule} from "@angular/material/button";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatCardModule} from "@angular/material/card";
 import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 const MaterialComponets = [
-   MatButtonModule,MatFormFieldModule,MatCardModule,MatInputModule
+   MatButtonModule,MatFormFieldModule,MatCardModule,MatInputModule,MatIconModule,MatSidenavModule,
+  MatDividerModule,MatToolbarModule
  ]
 @NgModule({
   declarations: [],
--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import {throwNullPortalOutletError} from "@angular/cdk/portal/portal-errors";
 
 @Injectable({
   providedIn: 'root'
